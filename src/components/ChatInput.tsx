becabb7531fbdf83ca8302d490340deb353c1f4c
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-08-11 07:16:48 -0700

    logic completed and now chat working well in continue I'll make some improvements

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -2,17 +2,43 @@
 import { Button, Textarea } from "@nextui-org/react";
 import { Send } from "lucide-react";
 import React from "react";
+import { type useChat } from "ai/react";
 
-const ChatInput = () => {
+type HandleInputChange = ReturnType<typeof useChat>["handleInputChange"];
+type HandleSubmit = ReturnType<typeof useChat>["handleSubmit"];
+type SetInput = ReturnType<typeof useChat>["setInput"];
+
+interface ChatInputProps {
+  input: string;
+  handleInputChange: HandleInputChange;
+  handleSubmit: HandleSubmit;
+  setInput: SetInput;
+}
+
+const ChatInput = ({
+  input,
+  handleInputChange,
+  handleSubmit,
+  setInput,
+}: ChatInputProps) => {
   return (
     <div className="z-10 bg-zinc-900 absolute bottom-0 left-0 w-full">
       <div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className="relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex flex-col w-full flex-grow p-4">
-            <form className="relative">
+            <form onSubmit={handleSubmit} className="relative">
               <Textarea
                 minRows={4}
                 autoFocus
+                onChange={handleInputChange}
+                value={input}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSubmit();
+                    setInput("");
+                  }
+                }}
                 placeholder="Enter your question ..."
                 className="resize-none bg-zinc-800 hover:bg-zinc-900 rounded-xl text-base"
               />
